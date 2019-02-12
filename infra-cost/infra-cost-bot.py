9148edf7428338faedc765f33707a8d0c3366b95
Brett Inman <brett.inman@docker.com>
2019-02-11 16:39:47 -0800

    Remove boilerplate

--- infra-cost/infra-cost-bot.py
+++ infra-cost/infra-cost-bot.py
@@ -1,31 +1,3 @@
-'''
-This function handles a Slack slash command for infra cost and returns 
-cost numbers for each infra provider.
-
-Follow these steps to configure the slash command in Slack:
-
-  1. Navigate to https://<your-team-domain>.slack.com/services/new
-
-  2. Search for and select "Slash Commands".
-
-  3. Enter a name for your command and click "Add Slash Command Integration".
-
-  4. Copy the token string from the integration settings and use it in the next section.
-
-  5. After you complete this blueprint, enter the provided API endpoint URL in the URL field.
-
-
-Follow these steps to complete the configuration of your command API endpoint
-
-  1. When completing the blueprint configuration select "Open" for security
-     on the "Configure triggers" page.
-
-  2. Enter a name for your execution role in the "Role name" field.
-
-  3. Update the URL for your Slack slash command with the invocation URL for the
-     created API resource in the prod stage.
-'''
-
 import boto3
 import json
 import logging
